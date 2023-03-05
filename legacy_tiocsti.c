55624ee37c62cf05a4d2a68a691e16e0a33c8e87
Kauruus <kauruus@outlook.com>
2023-03-05 17:59:21 +0800

    handle error

--- legacy_tiocsti.c
+++ legacy_tiocsti.c
@@ -18,19 +18,31 @@ static bool *tty_legacy_tiocsti;
 static int legacy_tiocsti_init(void)
 {
 	kallsyms_lookup_name_t kallsyms_lookup_name;
+	int ret;
 
-	register_kprobe(&kp);
+	ret = register_kprobe(&kp);
+	if (ret < 0) {
+		pr_err("legacy_tiocsti_init: kprobe failed");
+		return ret;
+	}
 	kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
 	unregister_kprobe(&kp);
 
 	tty_legacy_tiocsti = (void *)kallsyms_lookup_name("tty_legacy_tiocsti");
+	if (tty_legacy_tiocsti == NULL) {
+		pr_err("legacy_tiocsti_init: kallsyms_lookup_name failed to lookup tty_legacy_tiocsti");
+		return -1;
+	}
+
 	*tty_legacy_tiocsti = 1;
 	return 0;
 }
 
 static void legacy_tiocsti_cleanup(void)
 {
-	*tty_legacy_tiocsti = 0;
+	if (tty_legacy_tiocsti != NULL) {
+		*tty_legacy_tiocsti = 0;
+	}
 }
 
 module_init(legacy_tiocsti_init);
